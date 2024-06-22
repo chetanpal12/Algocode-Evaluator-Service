247e38ffe91bf46b1fda59ec6956e1cb3009bec5
chetan pal <cpal22387@gmail.com>
2024-06-23 00:16:44 +0530

    Added first job object and queue ->incomplete

---
+++ src/jobs/sampleJob.ts
@@ -0,0 +1,23 @@
+import { Job } from "bullmq";
+
+import { Ijob } from "../types/bullMqJobDefination";
+
+export default class SampleJob implements Ijob{
+    name:string;
+    payload: Record<string, unknown> | undefined;
+    constructor(payload:Record<string,unknown>){
+        this.name=this.constructor.name;
+        this.payload=payload;
+    }
+
+    handle=()=>{
+        console.log("Handler of the job called");
+
+    };
+    failed= (job?: Job) : void=>{
+        console.log("job failed");
+        if(job){
+            console.log(job.id);
+        }
+    }; 
+}
