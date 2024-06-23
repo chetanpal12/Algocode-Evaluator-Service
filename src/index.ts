839c0c4b0b651093cdc1b6858d07113257cd0164
chetan pal <cpal22387@gmail.com>
2024-06-23 12:34:57 +0530

    Bull mq based redis queue infra up and running with priority

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express,{Express} from 'express';
 
 import serverConfig from './config/serverConfig';
+import sampleQueueProducers from './producers/sampleQueueProducers';
 import apiRouter from './routes';
+import SampleWorker from './worker/sampleWorker';
 
 const app:Express=express();
 
@@ -9,4 +11,19 @@ app.use('/api',apiRouter);
 
 app.listen(serverConfig.PORT,()=>{
     console.log(`server started at port ${serverConfig.PORT}`);
+    SampleWorker('SampleQueue');
+
+    sampleQueueProducers('SampleJob', {
+        name: "Sarthak",
+        company: "swiggy",
+        position: "PM 2",
+        locatiion: "Noida"
+        },2);
+
+    sampleQueueProducers('SampleJob', {
+    name: "Sanket",
+    company: "Microsoft",
+    position: "SDE 2 L61",
+    locatiion: "Remote | BLR | Noida"
+    },1);
 });
