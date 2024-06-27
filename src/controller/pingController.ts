461e833959dfb337d677398051ef7d3d5a6887ad
chetan pal <cpal22387@gmail.com>
2024-06-27 21:46:33 +0530

    Integrated winston for logging

--- src/controller/pingController.ts
+++ src/controller/pingController.ts
@@ -1,7 +1,10 @@
 import { Request,Response } from "express";
 
+import logger from "../config/logger.config";
+
 export const pingCheck = (_:Request,res:Response)=>{
     // console.log("request url",req.url);
+    logger.error('hey winston is working');
     return res.status(200).json({
         message:'Ping check ok'
     });
